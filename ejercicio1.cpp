325a8ab6656e7c0e607786cda951ee2249dec8af
hdzzzvl <00126525@uca.edu.sv>
2025-06-05 11:37:32 -0600

    Ejercicio para parcial

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int opcion;
+    cout<<"Bienvenido al menu, seleccione una opcion:"<<endl;
+    cout<<"Opcion 1.\nOpcion 2.\nOpcion 3.\nOpcion 4."<<endl;
+    switch (opcion)
+
+
+
+
+
+
+    return 0;
+}
